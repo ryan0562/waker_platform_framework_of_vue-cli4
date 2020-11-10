6fbb17e1916470d4cec19788fac2d4aae01b6267
wangwei <519254178@qq.com>
2020-11-10 20:59:31 +0800

    vue原型添加映射表map

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * @Autor: Waker
  * @Date: 2020-11-10 17:05:10
  * @LastEditors: Waker
- * @LastEditTime: 2020-11-10 20:57:39
+ * @LastEditTime: 2020-11-10 20:59:08
  */
 import Vue from 'vue'
 import App from './App.vue'
@@ -12,7 +12,7 @@ import store from './store'
 
 Vue.config.productionTip = false
 /* vue原型添加映射表map */
-import map from '@/config/map/map.js'
+import map from '@/utils/map/map.js'
 Vue.prototype.$map = map
 
 /* 全局注册iconfont */
