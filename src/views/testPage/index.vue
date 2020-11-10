a34136b9f1176b7218e1fc6c6071c6f3dae7d72c
wangwei <519254178@qq.com>
2020-11-10 20:14:53 +0800

    添加文件别名

--- src/views/testPage/index.vue
+++ src/views/testPage/index.vue
@@ -3,11 +3,13 @@
  * @Autor: Waker
  * @Date: 2020-11-10 17:06:05
  * @LastEditors: Waker
- * @LastEditTime: 2020-11-10 17:43:03
+ * @LastEditTime: 2020-11-10 20:03:14
 -->
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png">
+    <img alt="Vue logo" src="@public/logo1.png">
+    <div class="bg"></div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,15 @@
  * @Autor: Waker
  * @Date: 2020-11-10 17:19:05
  * @LastEditors: Waker
- * @LastEditTime: 2020-11-10 18:39:27
+ * @LastEditTime: 2020-11-10 19:47:08
  */
 
 const path = require('path')
 // const webpack = require('webpack')
 
 
-function resolve (dir) {
-  return path.join(__dirname, dir)
+function resolve(dir) {
+    return path.join(__dirname, dir)
 }
 module.exports = {
     chainWebpack: config => {
@@ -24,8 +24,11 @@ module.exports = {
                 limit: 10240
             }))
         /* 将image改成base64 end*/
+        /* 添加文件夹别名 start */
         config.resolve.alias
-            .set('@$', resolve('src'))
+            .set('@', resolve('./src'))
+            .set('@public', resolve('./public'))
+        /* 添加文件夹别名 end */
     }
 
 }
