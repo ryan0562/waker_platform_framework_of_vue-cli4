ec545cfd710bfe5b318018768c7454f3ac9c5528
wangwei <wang-wei@hzzh.com>
2020-12-01 20:19:33 +0800

    权限改造

--- src/router/permission.js
+++ src/router/permission.js
@@ -25,23 +25,11 @@ const redirectLogin = (next) => {
   next('/login')
 }
 
-// 重定义next
-const reNext = (to, from, next) => {
-  // 重定向逻辑
-  const redirect = decodeURIComponent(from.query.redirect || to.path)
-  if (to.path === redirect) {
-    // 导航不会留下历史记录,回退的时候不需要点击两次
-    next({ ...to, replace: true })
-  } else {
-    // 跳转到目的路由
-    next({ path: redirect })
-  }
-}
-
 router.beforeEach((to, from, next) => {
   NProgress.start()
   // 页面标题
   document.title = to.meta.title ? `中恒VUE平台前端架构 - ${to.meta.title}` : '中恒VUE平台前端架构'
+
   // 通用路由直接进入
   if (whiteList.includes(to.name)) {
     next()
@@ -62,49 +50,7 @@ router.beforeEach((to, from, next) => {
     return
   }
 
-
-
-
-
-
-
-  // 没用户信息去请求用户信息
-  // if (!Vue.ls.get('user')) {
-  //   store.dispatch('GetInfo').then(res => {
-  //     debugger
-  //     if (lodash.isEmpty(store.getters.permission)) {
-  //       store.dispatch('GetPermission', {
-  //         userId: res.data.id,
-  //         companyId: store.getters.companyId,
-  //       }).then(perRes => {
-  //         store.dispatch('GenerateRoutes', perRes.dataList).then(() => {
-  //           // 根据roles权限生成可访问的路由表
-  //           // 动态添加可访问路由表
-  //           router.addRoutes(store.getters.addRouters)
-  //           // 请求带有 redirect 重定向时，登录自动重定向到该地址
-  //           const redirect = decodeURIComponent(from.query.redirect || to.path)
-  //           if (to.path === redirect) {
-  //             // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
-  //             next({ ...to, replace: true })
-  //           } else {
-  //             // 跳转到目的路由
-  //             next({ path: redirect })
-  //           }
-  //         })
-  //       }).catch((err) => {
-  //         notification.error({
-  //           message: '错误',
-  //           description: (err || {}).message || '请求用户权限列表失败',
-  //         })
-  //         store.dispatch('Logout').then(() => {
-  //           next({ path: '/login', query: { redirect: to.fullPath } })
-  //         })
-  //       })
-  //     }
-  //   })
-  // }
   next()
-
 })
 
 router.afterEach((to, from) => {
